ccf7cbc404b947cc3b68b6b870a15f5133257e23
Baokiin <bao.ho@staff.gumiviet.com>
2021-10-09 15:00:32 +0700

    create app

---
+++ app/src/main/java/com/baokiiin/hvcnbcvt/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.baokiiin.hvcnbcvt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
